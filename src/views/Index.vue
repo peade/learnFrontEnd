ed938a477e558be97d9c6902ab1ea7ead6e4eb7a
liuhefa <faheliu@163.com>
2019-01-31 20:59:41 +0800

    vue init project
    master rebase
    
    gulp 打包
    
    webpack server
    
    spritejs
    
    gulp 打包
    
    调整webpack 对应路径
    
    webpack
    
    remove spritejs
    add jquery
    
    Index
    
    dl dt dd
    
    vue 初始化
    
    vue 初始化项目
    
    ignore package-lock
    
    h2m
    
    master prettier
    
    Index 路由展示
    
    remove h2m
    
    Bump elliptic from 6.5.0 to 6.5.3
    
    Bumps [elliptic](https://github.com/indutny/elliptic) from 6.5.0 to 6.5.3.
    - [Release notes](https://github.com/indutny/elliptic/releases)
    - [Commits](https://github.com/indutny/elliptic/compare/v6.5.0...v6.5.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    add store
    
    update cli-service
    
    util tools
    
    xhr
    
    update
    
    add vant

---
+++ src/views/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Index
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Index',
+    filters: {},
+    components: {},
+    mixins: [],
+    props: {},
+    data() {
+      return {}
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    destroyed() {},
+    methods: {}
+  }
+</script>
+<style scoped lang="scss"></style>
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        implementation: require('sass')
+      }
+    }
+  },
+  devServer: {
+    open: true,
+    host: '0.0.0.0',
+    useLocalIp: true,
+    port: 3800
+    // 由于本项目数据通过easy-mock和mockjs模拟，不存在跨域问题，无需配置代理;
+    // proxy: {
+    //   '/v2': {
+    //       target: target,
+    //       changeOrigin: true
+    //   }
+    // }
+  }
+}
