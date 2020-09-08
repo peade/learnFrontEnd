b9526720bd30f70c2c7f7294120b0e63a38029a1
lhf <faheliu@163.com>
2020-09-08 10:05:16 +0800

    add jsonp

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -24,3 +24,42 @@ export function DeepClone(obj, hash = new WeakMap()) {
   }
   return cloneObj
 }
+
+// jsonp原理
+// jsonp 请求时带上callback参数，后端收到请求后，会用callback参数值对数据进行一次组装，
+// 例如 params.query.callback+'('+data+')'
+// 前端收到请求后，调用callback参数名相同的方法
+let jsonpId = 0
+export function jsonp(options) {
+  const container = document.body
+  if (!options || !options.url) {
+    return
+  }
+  let scriptNode = document.createElement('script')
+  let data = options.data || {}
+  let url = options.url
+  let callback = options.callback
+  let fnName = 'jsonp' + jsonpId++
+  data['callback'] = fnName
+  let params = []
+  for (let k in data) {
+    params.push(encodeURIComponent(k) + '=' + encodeURIComponent(data[k]))
+  }
+  url = url.indexOf('?') > 0 ? url + '&' : url + '?'
+  url += params.join('&')
+  scriptNode.src = url
+  // 执行回调方法，删除标签和全局函数
+  window[fnName] = function(res) {
+    callback && callback(res)
+    container.removeChild(scriptNode)
+    delete window[fnName]
+  }
+  // 请求出错处理
+  scriptNode.onerror = function() {
+    callback && callback({ error: 'error' })
+    container.removeChild(scriptNode)
+    window[fnName] && delete window[fnName]
+  }
+  scriptNode.type = 'text/javascript'
+  container.appendChild(scriptNode)
+}
