ac2bf3d794527de4194a73fc188c9b12e091f831
lhf <faheliu@163.com>
2020-09-08 11:42:57 +0800

    基本库学习

--- src/router/router-module.js
+++ src/router/router-module.js
@@ -4,5 +4,17 @@ export default [
     name:'Hello',
     component:()=> import('@/views/Hello'/* webpackChunkName: "Hello" */),
     meta:{title:'Hello World'}
-  }
+  },
+  {
+    path:'/pdf',
+    name:'Pdf',
+    component:()=> import('@/views/Pdf'/* webpackChunkName: "Pdf" */),
+    meta:{title:'Pdfjs'}
+  },
+  {
+    path:'/better-scroll',
+    name:'BetterScroll',
+    component:()=> import('@/views/BetterScrollDemo'/* webpackChunkName: "BetterScroll" */),
+    meta:{title:'BetterScroll'}
+  },
 ]
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,26 @@
 module.exports = {
-    devServer: {
-        open: true,
-        host: '0.0.0.0',
-        useLocalIp: true,
-        port: 3800
-        // 由于本项目数据通过easy-mock和mockjs模拟，不存在跨域问题，无需配置代理;
-        // proxy: {
-        //   '/v2': {
-        //       target: target,
-        //       changeOrigin: true
-        //   }
-        // }
-    },
+  devServer: {
+    open: true,
+    host: '0.0.0.0',
+    useLocalIp: true,
+    port: 3800
+    // 由于本项目数据通过easy-mock和mockjs模拟，不存在跨域问题，无需配置代理;
+    // proxy: {
+    //   '/v2': {
+    //       target: target,
+    //       changeOrigin: true
+    //   }
+    // }
+  },
+  chainWebpack(config) {
+    config.module
+      .rule('pdf')
+      .test(/\.pdf/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        outputPath: 'file',
+        name: '[name].[ext]'
+      })
+  }
 }
