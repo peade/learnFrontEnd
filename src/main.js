c6de5d1a712060e6addc9040cd1898b825f3967f
lhf <faheliu@163.com>
2020-09-28 19:12:09 +0800

    add vant

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-Vue.config.productionTip = false
+import vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(vant)
+Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
 new Vue({
   router,
