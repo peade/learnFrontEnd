bafb7a63fc44d375254972e9a64b53126ef010f3
lhf <faheliu@163.com>
2020-10-02 21:27:47 +0800

    deepclone

--- src/FrequentlyInterview/utils.js
+++ src/FrequentlyInterview/utils.js
@@ -36,3 +36,36 @@ export function protoInherit() {
   Child.prototype = Object.create(Parent.prototype)
   Child.prototype.constructor = Child
 }
+
+export function DeepClone(target, hash = new WeakMap()) {
+  // 对传入的参数处理
+  if (typeof target !== 'object' || target === null) {
+    return target
+  }
+  // 哈希表中存在，直接返回
+  if (hash.has(target)) {
+    return hash.get(target)
+  }
+  const cloneTarget = Array.isArray(target) ? [] : {}
+  hash.set(target, cloneTarget)
+  // 针对Symbol属性
+  const symKeys = Object.getOwnPropertySymbols(target)
+  if (symKeys.length) {
+    symKeys.forEach(symKey => {
+      if (typeof target[symKey] === 'object' && target[symKey] !== null) {
+        cloneTarget[symKey] = DeepClone(target[symKey])
+      } else {
+        cloneTarget[symKey] = target[symKey]
+      }
+    })
+  }
+  for (const i in target) {
+    if (Object.prototype.hasOwnProperty.call(target, i)) {
+      cloneTarget[i] =
+        typeof target[i] === 'object' && target[i] !== null
+          ? DeepClone(target[i], hash)
+          : target[i]
+    }
+  }
+  return cloneTarget
+}
