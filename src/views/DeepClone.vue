e2d57eaec541a7a91f38840ddfdbd32f79277ffb
lhf <faheliu@163.com>
2020-10-04 11:34:11 +0800

    merge lib-source-code

--- src/views/DeepClone.vue
+++ src/views/DeepClone.vue
@@ -4,24 +4,24 @@
   </div>
 </template>
 <script>
-import { DeepClone } from '@/utils/tools'
-export default {
-  name: 'DeepClone',
-  filters: {},
-  components: {},
-  mixins: [],
-  data() {
-    return {}
-  },
-  computed: {},
-  created() {},
-  mounted() {
-    const a = { v: 1, s: { m: 2 } }
-    const b = DeepClone(a)
-    console.log({ a, b })
-  },
-  destroyed() {},
-  methods: {}
-}
+  import { DeepClone } from '@/utils/tools'
+  export default {
+    name: 'DeepClone',
+    filters: {},
+    components: {},
+    mixins: [],
+    data() {
+      return {}
+    },
+    computed: {},
+    created() {},
+    mounted() {
+      const a = { v: 1, s: { m: 2 } }
+      const b = DeepClone(a)
+      console.log({ a, b })
+    },
+    destroyed() {},
+    methods: {}
+  }
 </script>
 <style scoped></style>
--- src/views/PeadePromise.vue
+++ src/views/PeadePromise.vue
@@ -2,33 +2,33 @@
   <div></div>
 </template>
 <script>
-import { PeaPromise } from '@/utils/pea-promise'
-export default {
-  name: 'PeadePromise',
-  filters: {},
-  components: {},
-  mixins: [],
-  data() {
-    return {}
-  },
-  computed: {},
-  created() {},
-  mounted() {},
-  destroyed() {},
-  methods: {
-    triggerPromise() {
-      PeaPromise.all([
-        new PeaPromise(resolve => {
-          setTimeout(() => {
-            console.log('resolve')
-            resolve()
-          }, 1000)
+  import { PeaPromise } from '@/utils/pea-promise'
+  export default {
+    name: 'PeadePromise',
+    filters: {},
+    components: {},
+    mixins: [],
+    data() {
+      return {}
+    },
+    computed: {},
+    created() {},
+    mounted() {},
+    destroyed() {},
+    methods: {
+      triggerPromise() {
+        PeaPromise.all([
+          new PeaPromise(resolve => {
+            setTimeout(() => {
+              console.log('resolve')
+              resolve()
+            }, 1000)
+          })
+        ]).then(() => {
+          console.log(2222)
         })
-      ]).then(() => {
-        console.log(2222)
-      })
+      }
     }
   }
-}
 </script>
 <style scoped></style>
