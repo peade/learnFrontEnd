54ff1f4c12191db2d9c848dd39630bc95f6b8864
lhf <faheliu@163.com>
2020-10-04 19:16:19 +0800

    is plain object

--- src/utils/jqTools.js
+++ src/utils/jqTools.js
@@ -10,3 +10,23 @@ export function DomEval(code) {
   script.text = code
   document.body.appendChild(code).parentNode.removeChild(script)
 }
+
+export function IsPlainObject(obj) {
+  const toString = {}.toString
+  if (!obj || toString.call(obj) !== '[object Object]') {
+    return false
+  }
+  let proto = Object.getPrototypeOf(obj)
+  // Objects with no prototype (e.g., `Object.create( null )`) are plain
+  if (!proto) {
+    return true
+  }
+  // Objects with prototype are plain iff they were constructed by a global Object function
+  const hasOwn = {}.hasOwnProperty
+  const fnToString = hasOwn.toString
+  const Ctor = hasOwn.call(proto, 'constructor') && proto.constructor
+  const ObjectFunctionString = fnToString.call(Object)
+  return (
+    typeof Ctor === 'function' && fnToString.call(Ctor) === ObjectFunctionString
+  )
+}
