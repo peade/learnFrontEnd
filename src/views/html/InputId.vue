4fde8dfd62994bb9ebfb69c1e8ccb30b411cead8
lhf <faheliu@163.com>
2021-02-18 16:47:25 +0800

    input id

---
+++ src/views/html/InputId.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input
+      type="text"
+      id="inputTest"
+      name="testInput"
+      value="input test value"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: 'InputId',
+  filters: {},
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {
+    setTimeout(() => {
+      console.dir(window.inputTest)
+      console.log(window.inputTest.value)
+    }, 100)
+  },
+  destroyed() {},
+  methods: {}
+}
+</script>
+<style scoped lang="scss"></style>
