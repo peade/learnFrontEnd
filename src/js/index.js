b0a51ca055df2e7bb6ca4ecfa683324061d79102
lhf <faheliu@163.com>
2021-02-26 13:03:59 +0800

    test

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,17 @@
 var block = 122
 var c = 123
 console.log(block, c)
-setTimeout(function () {
-  location.href = 'index2.html'
-}, 3000)
+/* eslint-disable-next-line */
+function testDemo(ab, cd) {
+  console.log('demo ', demo)
+  console.log('demo2', demo2)
+  console.log('ab', 'cd')
+  /* eslint-disable-next-line */
+  return
+}
+
+var demo = 111
+
+const demo2 = 222
+
+console.dir(testDemo)
