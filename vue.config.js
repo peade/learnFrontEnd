ba2907d7e18f833cd20d77e272885c139cee44a5
peade <peade@live.cn>
2021-04-03 21:30:25 +0800

    config disable host check

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,7 @@ module.exports = {
     open: true,
     host: '0.0.0.0',
     useLocalIp: true,
+    disableHostCheck: true,
     port: 3800
     // 由于本项目数据通过easy-mock和mockjs模拟，不存在跨域问题，无需配置代理;
     // proxy: {
